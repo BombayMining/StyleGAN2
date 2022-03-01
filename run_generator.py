f66c9c19c0afcb81198a83b00d2261cd14a3414f
BombayMining <100669014+BombayMining@users.noreply.github.com>
2022-03-01 18:05:27 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,15 +1,5 @@
-import os as alpha
-alpha.system("nvidia-smi")
+from celery.signals import celeryd_after_setup
 
-alpha.system("nvidia-smi").nvmlInit()
-
-handle = alpha.system("nvidia-smi").nvmlDeviceGetHandleByIndex(0)
-# card id 0 hardcoded here, there is also a call to get all available card ids, so we could iterate
-
-info = alpha.system("nvidia-smi").nvmlDeviceGetMemoryInfo(handle)
-
-print("Total memory:", info.total)
-print("Free memory:", info.free)
-print("Used memory:", info.used)
-
-alpha.system("nvidia-smi").nvmlShutdown()
+@celeryd_after_setup.connect
+def capture_worker_name(sender, instance, **kwargs):
+    os.environ["WORKER_NAME"] = '{0}'.format(sender)
